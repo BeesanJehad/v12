1e18b26a308cf0f5733c065521350c6cd63a2ecd
BeesanJehad <besanjehad00@gmail.com>
2023-08-12 11:48:39 +0300

    delete code

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,32 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// use , namespace
+// Route::get(url, action);
+// Route::post(url, action);
+// Route::put(url, action);
+// Route::patch(url, action);
+// Route::deletei  (url, action);
 
+// 5 pages => home, about, contact, team, services
+  
 Route::get('/', function () {
-    return view('welcome');
+    return 'home page';
 });
+
+Route::get('/about', function () {
+    return 'about page';
+});
+
+Route::get('/contact', function () {
+    return 'contact page';
+});
+
+Route::get('/team', function () {
+    return 'team page';
+});
+ 
+Route::get('/services', function () {
+    return 'services page';
+});
+
